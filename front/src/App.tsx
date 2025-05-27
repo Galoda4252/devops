05eb3ce61f714d01a4ecfbbefb8c6edad73b668d
Juricaa <harimamidina@gmail.com>
2025-05-27 12:32:48 +0300

    ttt

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,7 +4,10 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const increment = ()=>{
+    setCount(count)
+  }
 
   return (
     <>
@@ -18,7 +21,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={increment}>
           count is {count}
         </button>
         <p>
