d4bb87c895df339d532ba277040bd3cb1ffb2543
Galoda4252 <saphiraroger08@gmail.com>
2025-05-27 13:33:34 +0200

    build

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={increment}>
-          counteur mety {count}
+          countera {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
